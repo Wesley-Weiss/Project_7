e0e0f6c9c457f61515600b87c8a369971280000b
Wesley Weiss <weisswesley22@gmail.com>
2025-05-31 02:37:08 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,79 @@
+const numberInput = document.getElementById("number");
+const convertBtn = document.getElementById("convert-btn");
+const output = document.getElementById("output");
+
+const romanNumerals = [
+  "M",
+  "CM",
+  "D",
+  "CD",
+  "C",
+  "XC",
+  "L",
+  "XL",
+  "X",
+  "IX",
+  "V",
+  "IV",
+  "I"
+];
+
+const arabicNumerals = [
+  1000,
+  900,
+  500,
+  400,
+  100,
+  90,
+  50,
+  40,
+  10,
+  9,
+  5,
+  4,
+  1
+];
+
+let romanNumeralString = "";
+
+const inputChecker = number => {
+  const num = parseInt(number);
+  output.innerText = "";
+  romanNumeralString = "";
+
+  if (!num) {
+    output.innerText = "Please enter a valid number";
+    return;
+  } else if (num < 1) {
+    output.innerText = "Please enter a number greater than or equal to 1";
+    return;
+  } else if (num > 3999) {
+    output.innerText = "Please enter a number less than or equal to 3999";
+    return;
+  } else {
+    converter(num);
+  }
+}
+
+const converter = number => {
+ 
+
+  arabicNumerals.forEach((num, index)=> {
+    while (number >= num) {
+      number -= num;
+      romanNumeralString += romanNumerals[index];
+    }
+  })
+
+  output.innerText = romanNumeralString;
+}
+
+convertBtn.addEventListener("click", () => {
+  inputChecker(numberInput.value);
+});
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    inputChecker(numberInput.value);
+  }
+});
